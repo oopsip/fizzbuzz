f6f63e65be6f8014d665898c2a267289570f2a70
oopsip <srujirawat@sitthipols-MacBook-Pro.local>
2014-10-20 16:07:08 -0700

    Initial commit

---
+++ fizzbuzz_v1.py
@@ -0,0 +1,10 @@
+print "Fizz buzz counting up to 100"
+for x in range(1,101):
+    if (x%3 ==0) and (x%5 == 0):
+        print "Fizz Buzz"
+    elif x%3 == 0:
+        print "Fizz"
+    elif x%5 == 0:
+    	print "Buzz"
+    else:
+    	print x
